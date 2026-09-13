13f3f41f545301d0f90b6f23ab7e3b55526f6753
srivardhan <jillasrivardhan@gmail.com>
2026-09-13 17:36:50 +0530

    added some requirements and added langchain models

---
+++ langchain-models/llms.py
@@ -0,0 +1,12 @@
+
+from langchain_ollama import ChatOllama
+
+
+llm = ChatOllama(
+    model="qwen2.5:3b",
+    temperature=0
+)
+
+response = llm.invoke("Explain ai agents in 10 words.")
+
+print(response.content)
